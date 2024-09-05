6ae7b1861b1b93afae1cfadcb97282d5270e7200
pablorenan-dev <pablonichterwistz@outlook.com>
2024-09-05 18:36:13 -0300

    script de traduzir as siglas do ingles para o portugues esta finalizada

--- indexScript.js
+++ indexScript.js
@@ -4,4 +4,26 @@ setTimeout(function traduzirSiglasWDL() {
   console.log(todasSiglas, "tudo");
   let sigla = todasSiglas[3].children;
   console.log(sigla[0].innerHTML);
+
+  for (let i = 0; i < todasSiglas.length; i++) {
+    let siglasChildren = todasSiglas[i].children;
+
+    for (let j = 0; j < siglasChildren.length; j++) {
+      let siglaChildConteudo = siglasChildren[j].innerHTML;
+
+      switch (siglaChildConteudo) {
+        case "W":
+          siglasChildren[j].innerHTML = "V";
+          break;
+
+        case "L":
+          siglasChildren[j].innerHTML = "X";
+          break;
+
+        case "D":
+          siglasChildren[j].innerHTML = "E";
+          break;
+      }
+    }
+  }
 }, 2000); // 2000 milissegundos = 2 segundos
